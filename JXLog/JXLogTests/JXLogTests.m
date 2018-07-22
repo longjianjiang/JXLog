3eab4dbfa123977351c2e1a33756b96d9fc57dcc
longjianjiang <616393956@qq.com>
2018-07-22 16:07:36 +0800

    write readme

--- JXLog/JXLogTests/JXLogTests.m
+++ JXLog/JXLogTests/JXLogTests.m
@@ -45,4 +45,14 @@
     JXLogError(@"可以看到上面的 Log 信息中只有前 5 个，后面因为打印开关关闭而停止了 log");
 }
 
+- (void)testDomainLog {
+    NSString *domain = @"JXTestDomain";
+    [JXLogger addLoggerWithDomain:domain];
+    JXFlagError(domain, @"这是一个高级用法，可以控制是否打印方法，并且需要绑定一个 domain");
+    JXFlagWarning(domain, @"如果 domain 没有绑定，那么使用该 domain 的 Log 信息是不会输出的");
+    JXFlagDebug(domain, @"目前的 domain 是: %@", domain);
+    JXFlagInfo(domain, @"下面会用一个没有注册的 domain 来测试打印 --> JXFlagVerbose(YES, @\"OtherDomain\", @\"看不到我看不到我看不到我\");");
+    JXFlagVerbose(@"OtherDomain", @"看不到我看不到我看不到我");
+    JXFlagInfo(domain, @"看行号，发现少了一行，因为上一个没有注册 domain 没有打印");
+}
 @end
